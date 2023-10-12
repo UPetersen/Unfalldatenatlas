9e01a13f2657dd98afe9d30021733c3c53fe4e74
UPetersen <uwe.n.petersen@icloud.com>
2023-10-12 19:34:12 +0200

    Added percentage information to count of selected accidents, which now read e.g. as "1.554.384 Unfälle (100 %)"

--- CountOfSelectedAccidentsView.swift
+++ CountOfSelectedAccidentsView.swift
@@ -7,21 +7,43 @@
 
 import SwiftUI
 
+fileprivate let formatter: NumberFormatter =  {() -> NumberFormatter in
+    let numberFormatter = NumberFormatter()
+    numberFormatter.usesSignificantDigits = true
+    numberFormatter.maximumSignificantDigits = 2
+    numberFormatter.roundingMode = .halfUp
+    numberFormatter.zeroSymbol = "0"
+    return numberFormatter
+}()
+
 /// Displays count of selected accidents or a ProgressView while the count is still being fetched.
 struct CountOfSelectedAccidentsView: View {
     
     var countOfSelectedAccidents: Int
     var isFetchingCountOfSelectedAccidents: Bool
+    let countOfAllAccidents = Double(ViewModel.countOfAllAccidents)
     
     var body: some View {
         if isFetchingCountOfSelectedAccidents {
             ProgressView()
         } else {
-            Text("\(countOfSelectedAccidents)")
+            Text("\(countOfSelectedAccidents) Unfälle\(percentage())")
         }
+    }
+    
+    func percentage() -> String {
+        if countOfAllAccidents > 1 { // Never divide by zero
+            let percentage = Double(countOfSelectedAccidents) * 100.0 / countOfAllAccidents
+            if let text = formatter.string(from: NSNumber(value: percentage)) {
+                return " (\(text) %)"
+            }
+        }
+        return ""
     }
 }
 
 #Preview {
-    CountOfSelectedAccidentsView(countOfSelectedAccidents: 1554834, isFetchingCountOfSelectedAccidents: false)
+//    CountOfSelectedAccidentsView(countOfSelectedAccidents: Int(15.54834), isFetchingCountOfSelectedAccidents: false)
+    CountOfSelectedAccidentsView(countOfSelectedAccidents: Int(1), isFetchingCountOfSelectedAccidents: false)
+//    CountOfSelectedAccidentsView(countOfSelectedAccidents: 1554834, isFetchingCountOfSelectedAccidents: false)
 }
